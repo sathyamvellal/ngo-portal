5f4fc031807e107c7da4f47e5c7f0625633b3e88
Sathyam M Vellal <sathyam.vellal@gmail.com>
2013-05-11 21:10:09 +0530

    Now supporting openid login
    The following changes have been made to support the openid
    login functionality through flask login
    + necessary imports
    + openid login handler
    + logout handler
    + other necessary methods
    	(after_login)

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,5 @@
 # Local project imports
-from core import app, lm
+from core import app, lm, oid
 from core.models import User
 from core.forms import LoginForm
 import helpers as util
@@ -8,7 +8,7 @@ import helpers as util
 import json
 
 # Third party imports
-from flask import render_template, jsonify, request, g
+from flask import redirect, url_for, render_template, jsonify, request, g
 from flask.ext.login import login_required, login_user, logout_user, current_user
 
 # Application homepage
@@ -17,11 +17,38 @@ from flask.ext.login import login_required, login_user, logout_user, current_use
 def index():
     return render_template("index.html")
 
+# Application Login Page
 @app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
-	if g.user is not None and g.user.is_authenticated():
-		pass 
-	return render_template('/login.html', form = LoginForm())
+    if g.user is not None and g.user.is_authenticated():
+        # TODO: Already logged in. Move to a default after-login page.
+        # For now asks to logout
+        return 'Now logged in <a href="logout">Logout</a>'
+    form = LoginForm()
+    if form.validate_on_submit():
+        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+    return render_template('/login.html', form = form)
+
+# Flask-OpenID callback
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        # TODO: action for empty email. For now redirecting back to Login
+        # Although I think this case won't appear because of WTF
+        flash('Invalid Login, please try again')
+        redirect(url_for('login'))
+    user = User.query.filter_by(email = resp.email).first()
+    if user is None:
+        return 'Gone! Not logged in' # TODO: action for no user with such credentials
+    login_user(user)
+    return 'Now logged in <a href="logout">Logout</a>'
+
+# Application Logout
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -61,11 +88,11 @@ def contact():
 # Other necessary functions
 @lm.user_loader
 def user_loader(id):
-	return User.query.get(int(id))
+    return User.query.get(int(id))
 
 @app.before_request
 def before_request():
-	g.user = current_user;
+    g.user = current_user;
 
 # Application error handlers
 @app.errorhandler(400)
